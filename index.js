db48f3b9fff2b9e70cbe50ed44c7e7cba63e0ae3
ali <alimahmoud202433@gmail.com>
2025-05-22 04:48:02 -0700

    editing index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const btn = document.getElementById("btn")
 
 let card = document.getElementById("card1")
+
+
+btn.addEventListener("click", (e) => {
+
+
+    console.log(e.target)
+
+
+
+})
+
+
+window.addEventListener("load", function() {
+    card.style.display = 'none'
+})
